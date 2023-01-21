57a146040755f7f4d28af008c4f818fbf85a968c
RafaEscobar <rafael.eg.code@gmail.com>
2023-01-20 21:28:07 -0600

    Evidencia agregada

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Crud-2-Laravel-propio
 CRUD de animes (no me juzguen si leen esto) con imágenes, Este proyecto será base para proyectos académicos y propios... que ironía xd.
+
+<h3>Ilustraciones</h3>
+<br />
+<h4 align="center"> Listar </h4>
+<p align="center"><img src="https://i.postimg.cc/bJCQyrR2/a.png" width="800" height="400"></p>
+<h4 align="center"> Crear </h4>
+<p align="center"><img src="https://i.postimg.cc/3wZXqn41/b.png" width="800" height="400"></p>
+<h4 align="center"> Actualizar </h4>
+<p align="center"><img src="https://i.postimg.cc/QtxQZ1c3/d.png" width="800" height="450"></p>
+<h4 align="center"> Mostrar </h4>
+<p align="center"><img src="https://i.postimg.cc/wBz5FSv3/c.png" width="800" height="400"></p>
--- resources/views/animes/read.blade.php
+++ resources/views/animes/read.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Listado de registros')
 
 @section('content')
-    <h1>Listado de registros de animes</h1>
+    <h1 class="text-3xl mb-8">Listado de registros de animes</h1>
     <div class="relative overflow-x-auto">
+      <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" ><a href="{{route('animes.create')}}">Crear nuevo registro</a>
+      </button>
       <table class="border-collapse border border-gray-800 w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-900 uppercase dark:text-gray-700">
               <tr>
